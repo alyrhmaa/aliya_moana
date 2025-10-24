806a87cd87bcbb6560e228968d20105ac751df5d
naaila24sia <aliya24si@mahasiswa.pcr.ac.id>
2025-10-24 10:52:22 +0700

    pertemuan 7

--- resources/views/admin/user/create.blade.php
+++ resources/views/admin/user/create.blade.php
@@ -12,13 +12,13 @@
                             </svg>
                         </a>
                     </li>
-                    <li class="breadcrumb-item"><a href="#">Pelanggan</a></li>
-                    <li class="breadcrumb-item active" aria-current="page">Tambah Pelanggan</li>
+                    <li class="breadcrumb-item"><a href="#">User</a></li>
+                    <li class="breadcrumb-item active" aria-current="page">Tambah User</li>
                 </ol>
             </nav>
             <div class="d-flex justify-content-between w-100 flex-wrap">
                 <div class="mb-3 mb-lg-0">
-                    <h1 class="h4">Tambah Pelanggan</h1>
+                    <h1 class="h4">Tambah User</h1>
                     <p class="mb-0">Form untuk menambahkan data pelanggan baru.</p>
                 </div>
                 <div>
@@ -31,53 +31,34 @@
             <div class="col-12 mb-4">
                 <div class="card border-0 shadow components-section">
                     <div class="card-body">
-                        <form action="{{route('pelanggan.store')}}" method="POST">
+                        <form action="{{route('user.store')}}" method="POST">
                             @csrf
                             <div class="row mb-4">
                                 <div class="col-lg-4 col-sm-6">
-                                    <!-- First Name -->
+                                    <!-- Nama Lengkap -->
                                     <div class="mb-3">
-                                        <label for="first_name" class="form-label">First name</label>
-                                        <input type="text" name="first_name" id="first_name" class="form-control" required>
+                                        <label for="name" class="form-label">Nama Lengkap</label>
+                                        <input type="text" name="name" id="name" class="form-control" required>
                                     </div>
 
-                                    <!-- Last Name -->
-                                    <div class="mb-3">
-                                        <label for="last_name" class="form-label">Last name</label>
-                                        <input type="text" name="last_name" id="last_name" class="form-control" required>
-                                    </div>
-                                </div>
-
-                                <div class="col-lg-4 col-sm-6">
-                                    <!-- Birthday -->
-                                    <div class="mb-3">
-                                        <label for="birthday" class="form-label">Birthday</label>
-                                        <input type="date" name="birthday" id="birthday" class="form-control">
-                                    </div>
-
-                                    <!-- Gender -->
-                                    <div class="mb-3">
-                                        <label for="gender" class="form-label">Gender</label>
-                                        <select id="gender" name="gender" class="form-select">
-                                            <option value="">-- Pilih --</option>
-                                            <option value="Male">Male</option>
-                                            <option value="Female">Female</option>
-                                            <option value="Other">Other</option>
-                                        </select>
-                                    </div>
-                                </div>
-
-                                <div class="col-lg-4 col-sm-12">
                                     <!-- Email -->
                                     <div class="mb-3">
                                         <label for="email" class="form-label">Email</label>
                                         <input type="text" name="email" id="email" class="form-control" required>
                                     </div>
+                                </div>
 
-                                    <!-- Phone -->
+                                <div class="col-lg-4 col-sm-12">
+                                    <!-- Password -->
                                     <div class="mb-3">
-                                        <label for="phone" class="form-label">Phone</label>
-                                        <input type="text" name="phone" id="phone" class="form-control">
+                                        <label for="password" class="form-label">Password</label>
+                                        <input type="password" name="password" id="password" class="form-control" required>
+                                    </div>
+
+                                    <!-- Confirmation Password -->
+                                    <div class="mb-3">
+                                        <label for="password_confirmation" class="form-label">Confirm Password</label>
+                                        <input type="password" name="password_confirmation" id="password_confirmation" class="form-control">
                                     </div>
 
                                     <!-- Buttons -->
--- resources/views/admin/user/index.blade.php
+++ resources/views/admin/user/index.blade.php
@@ -16,17 +16,17 @@
                             </svg>
                         </a>
                     </li>
-                    <li class="breadcrumb-item"><a href="#">Pelanggan</a></li>
+                    <li class="breadcrumb-item"><a href="#">User</a></li>
                 </ol>
             </nav>
             <div class="d-flex justify-content-between w-100 flex-wrap">
                 <div class="mb-3 mb-lg-0">
-                    <h1 class="h4">Data Pelanggan</h1>
+                    <h1 class="h4">Data User</h1>
                     <p class="mb-0">List data seluruh pelanggan</p>
                 </div>
                 <div>
-                    <a href="{{ route('pelanggan.create') }}" class="btn btn-success text-white"><i
-                            class="far fa-question-circle me-1"></i> Tambah Pelanggan</a>
+                    <a href="{{ route('user.create') }}" class="btn btn-success text-white"><i
+                            class="far fa-question-circle me-1"></i> Tambah User</a>
                 </div>
             </div>
         </div>
@@ -39,27 +39,23 @@
                             <table id="table-pelanggan" class="table table-centered table-nowrap mb-0 rounded">
                                 <thead class="thead-light">
                                     <tr>
-                                        <th class="border-0">First Name</th>
-                                        <th class="border-0">Last Name</th>
-                                        <th class="border-0">Birthday</th>
-                                        <th class="border-0">Gender</th>
+                                        <th class="border-0">Nama Lengkap</th>
                                         <th class="border-0">Email</th>
-                                        <th class="border-0">Phone</th>
+                                        <th class="border-0">Password</th>
+
                                         <th class="border-0 rounded-end">Action</th>
                                     </tr>
                                 </thead>
                                 <tbody>
 
-                                    @foreach ($dataPelanggan as $item)
+                                    @foreach ($dataUser as $item)
                                         <tr>
-                                            <td>{{ $item->first_name }}</td>
-                                            <td>{{ $item->last_name }}</td>
-                                            <td>{{ $item->birthday }}</td>
-                                            <td>{{ $item->gender }}</td>
-                                            <td>{{ $item->email }}</td>
-                                            <td>{{ $item->phone }}</td>
+                                            <td>{{ $item->name }}</td>
+                                            <td>{{ $item->email}}</td>
+                                            <td>{{ $item->password }}</td>
+
                                             <td>
-                                                <a href="{{ route('pelanggan.edit', $item->pelanggan_id) }}"
+                                                <a href=""
                                                     class="btn btn-info btn-sm">
                                                     <svg class="icon icon-xs me-2" data-slot="icon" fill="none"
                                                         stroke-width="1.5" stroke="currentColor" viewBox="0 0 24 24"
@@ -70,7 +66,7 @@
                                                     </svg>
                                                     Edit
                                                 </a>
-                                                <form action="{{route('pelanggan.destroy', $item->pelanggan_id)}}" method="POST" style="display:inline">
+                                                <form action="" method="POST" style="display:inline">
                                                     @csrf
                                                     @method('DELETE')
                                                     <button type="submit" class="btn btn-danger btn-sm">
