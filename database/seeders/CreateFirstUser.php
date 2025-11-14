3bd2f507aa439ae9be09d3035acf85d8ddee8783
pangestuasli <aliya24si@mahasiswa.pcr.ac.id>
2025-11-14 09:03:34 +0700

    seeder

--- database/seeders/CreateFirstUser.php
+++ database/seeders/CreateFirstUser.php
@@ -1,11 +1,9 @@
 <?php
-
 namespace Database\Seeders;
 
 use App\Models\User;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\Hash;
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 
 class CreateFirstUser extends Seeder
 {
@@ -14,10 +12,10 @@ class CreateFirstUser extends Seeder
      */
     public function run(): void
     {
-                User::create([
-            'name'=> 'Admin',
-            'email' => 'gatot@pcr.ac.id',
-            'password' => Hash::make('gatotkaca')
+        User::create([
+            'name'     => 'Admin',
+            'email'    => 'gatot@pcr.ac.id',
+            'password' => Hash::make('gatotkaca'),
         ]);
     }
 }
